d3c37407e8582b9a488fc1a05892f6d20084c805
sudhir <sudhir.suvarna@mq.edu.au>
2026-01-16 16:29:48 +1100

    new storage account details

--- terraformtesting/storageterraformworkspces/provider.tf
+++ terraformtesting/storageterraformworkspces/provider.tf
@@ -1,9 +1,26 @@
-provider "azurerm" {
-  features {}
+terraform {
+  required_version = ">= 1.8.0"
 
-#  subscription_id = "0f1c21d3-1985-4b39-b41e-059f11f4d5ce"
-#  tenant_id       = "7ad7e92b-f714-4f87-8b0c-297eefcad1b3"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "~> 0.57.0"
+    }
+  }
 
-  # IMPORTANT:
-  # Do NOT set client_id/use_oidc/oidc_token here when using TFC dynamic creds.
+  cloud {
+    organization = "sidazureorg"
+
+    workspaces {
+      name = "azuredevops" # Blast radius restriction
+    }
+  }
 }
