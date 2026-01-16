814874f6ed2d6cd5a4b2715b8c427ec3991f9152
sudhir <sudhir.suvarna@mq.edu.au>
2026-01-16 17:12:42 +1100

    changed the locals.tf file

--- terraformtesting/storageterraformworkspces/locals.tf
+++ terraformtesting/storageterraformworkspces/locals.tf
@@ -1,11 +1,10 @@
-
 locals {
   # The active Terraform workspace is the environment
   env = terraform.workspace
 
   # Per-workspace configuration
   env_config = {
-    azuredevops = {
+    dev = {
       location             = "australiaeast"
       resource_group_name  = "rg-storage-dev"
       storage_account_name = "ststoragedev01"
@@ -15,9 +14,27 @@ locals {
       }
     }
 
-  
+    uat = {
+      location             = "australiaeast"
+      resource_group_name  = "rg-storage-uat"
+      storage_account_name = "ststorageuat01"
+      tags = {
+        environment = "uat"
+        managed_by  = "terraform"
+      }
+    }
+
+    prod = {
+      location             = "australiaeast"
+      resource_group_name  = "rg-storage-prod"
+      storage_account_name = "ststorageprod01"
+      tags = {
+        environment = "prod"
+        managed_by  = "terraform"
+      }
+    }
+  }
 
   # Select config for the active workspace
-  current = azuredevops
-}
+  current = local.env_config[local.env]
 }
